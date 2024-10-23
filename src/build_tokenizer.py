ad37526b5cca1e7a18f5ac2553dc628a906136a7
e-hossam96 <ehh.hossam@gmail.com>
2024-10-23 07:06:42 +0300

    added code to train custom tokenizer from base gpt2

---
+++ src/build_tokenizer.py
@@ -0,0 +1,82 @@
+"""
+Training and saving the gpt2-based tokenizer on preprocessed dataset.
+"""
+
+import pathlib
+import argparse
+from rich import print
+from datasets import load_dataset
+from transformers import AutoTokenizer
+
+
+def get_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(
+        description="Argument Parser for tokenizer training configurations."
+    )
+    # Define arguments
+    parser.add_argument(
+        "--model_ckpt",
+        type=str,
+        default="openai-community/gpt2",
+        help="Checkpoint for the model. (default: 'openai-community/gpt2')",
+    )
+    parser.add_argument(
+        "--data_ckpt",
+        type=str,
+        default="wikimedia/wikipedia",
+        help="Checkpoint for the dataset. (default: 'wikimedia/wikipedia')",
+    )
+    parser.add_argument(
+        "--processed_data_file_path",
+        type=pathlib.Path,
+        default=pathlib.Path(f"data/wikimedia/wikipedia.csv"),
+        help="Path to the processed data CSV file. (Default: 'data/{data_ckpt}.csv')",
+    )
+    parser.add_argument(
+        "--model_max_length",
+        type=int,
+        default=1024,
+        help="Model maximum context length. (default: 1024)",
+    )
+    parser.add_argument(
+        "--vocab_size",
+        type=int,
+        default=8192,
+        help="Tokenizer vocab size. (default: 8192)",
+    )
+    parser.add_argument(
+        "--iterator_batch_size",
+        type=int,
+        default=1024,
+        help="Tokenizer training iterator batch size. (default: 1024)",
+    )
+    # Parse the arguments
+    args = parser.parse_args()
+    # Handle dynamic default for `target_text_file_path` based on `data_ckpt`
+    if args.processed_data_file_path == pathlib.Path(f"data/{{data_ckpt}}.csv"):
+        args.processed_data_file_path = pathlib.Path(f"data/{args.data_ckpt}.csv")
+    return args
+
+def main(args: argparse.Namespace) -> None:
+    # loading the dataset
+    dataset = load_dataset("csv", data_files=args.processed_data_file_path)
+    print(f"Loaded dataset\n{dataset}")
+    print(f"Sample record\n{dataset['train'][0]}")
+    # loading the base model tokenizer
+    base_tokenizer = AutoTokenizer.from_pretrained(args.model_ckpt, model_max_length=args.model_max_length)
+    print(f"Base tokenizer\n{base_tokenizer}")
+    # training tokenizer
+
+    def get_dataset_iterator(data, batch_size: int = 1024):
+        for i in range(0, len(data), batch_size):
+            yield data[i : i + batch_size]["text"]
+
+    dataset_iterator = get_dataset_iterator(dataset["train"], batch_size=args.iterator_batch_size)
+    tokenizer = base_tokenizer.train_new_from_iterator(dataset_iterator, vocab_size=args.vocab_size)
+    print(f"New tokenizer\n{tokenizer}")
+    tokenizer.save_pretrained(args.target_model_path)
+    print(f"Saved tokenizer to path: {args.target_model_path}")
+
+if __name__ == "__main__":
+    args = get_args()
+    main(args)
