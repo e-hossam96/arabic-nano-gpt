2f244d2af4de0cb058dcb8c42557f8d3cd2349c8
e-hossam96 <ehh.hossam@gmail.com>
2024-10-23 12:30:10 +0300

    added some annotations for training code

--- train_lm.sh
+++ train_lm.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 python src/preprocess_data.py
 python src/build_tokenizer.py
-python src/train_causal_lm.py
+python src/train_causal_lm.py \
+    --split_size=200000 \
+    --num_epochs=2 \
+    --lr=6e-4 \
+    --run_name="Arabic-NanoGPT-LM-on-200K-Docs" \
+    --notes="LM Training on Arabic Data using Nano GPT2 Model Architecture" \
+    --tags=["MOdeling", "Transformers", "GPT2", "Language-Modeling", "Arabic"]
