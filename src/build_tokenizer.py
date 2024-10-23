854a89465df0f871c96b20ba83bd402afdea0398
e-hossam96 <ehh.hossam@gmail.com>
2024-10-23 07:27:10 +0300

    added more args to arg paperser and fixed naming erros

--- src/build_tokenizer.py
+++ src/build_tokenizer.py
@@ -18,13 +18,13 @@ def get_args() -> argparse.Namespace:
         "--model_ckpt",
         type=str,
         default="openai-community/gpt2",
-        help="Checkpoint for the model. (default: 'openai-community/gpt2')",
+        help="Checkpoint for the base model. (default: 'openai-community/gpt2')",
     )
     parser.add_argument(
         "--data_ckpt",
         type=str,
         default="wikimedia/wikipedia",
-        help="Checkpoint for the dataset. (default: 'wikimedia/wikipedia')",
+        help="Checkpoint for the original dataset. (default: 'wikimedia/wikipedia')",
     )
     parser.add_argument(
         "--processed_data_file_path",
@@ -50,20 +50,38 @@ def get_args() -> argparse.Namespace:
         default=1024,
         help="Tokenizer training iterator batch size. (default: 1024)",
     )
+    parser.add_argument(
+        "--model_name",
+        type=str,
+        default="arabic-nano-gpt",
+        help="To be trained model name. (default: 'arabic-nano-gpt')",
+    )
+    parser.add_argument(
+        "--target_model_path",
+        type=pathlib.Path,
+        default=pathlib.Path(f"models/{{model_name}}"),
+        help="Path to the end model. (Default: 'models/{model_name}')",
+    )
     # Parse the arguments
     args = parser.parse_args()
-    # Handle dynamic default for `target_text_file_path` based on `data_ckpt`
+    # Handle dynamic default for `processed_data_file_path` based on `data_ckpt`
     if args.processed_data_file_path == pathlib.Path(f"data/{{data_ckpt}}.csv"):
         args.processed_data_file_path = pathlib.Path(f"data/{args.data_ckpt}.csv")
+    # Handle dynamic default for `target_model_path` based on `model_name`
+    if args.target_model_path == pathlib.Path(f"models/{{model_name}}"):
+        args.target_model_path = pathlib.Path(f"models/{args.model_name}")
     return args
+
 
 def main(args: argparse.Namespace) -> None:
     # loading the dataset
-    dataset = load_dataset("csv", data_files=args.processed_data_file_path)
+    dataset = load_dataset("csv", data_files=[str(args.processed_data_file_path)])
     print(f"Loaded dataset\n{dataset}")
     print(f"Sample record\n{dataset['train'][0]}")
     # loading the base model tokenizer
-    base_tokenizer = AutoTokenizer.from_pretrained(args.model_ckpt, model_max_length=args.model_max_length)
+    base_tokenizer = AutoTokenizer.from_pretrained(
+        args.model_ckpt, model_max_length=args.model_max_length
+    )
     print(f"Base tokenizer\n{base_tokenizer}")
     # training tokenizer
 
@@ -71,11 +89,17 @@ def main(args: argparse.Namespace) -> None:
         for i in range(0, len(data), batch_size):
             yield data[i : i + batch_size]["text"]
 
-    dataset_iterator = get_dataset_iterator(dataset["train"], batch_size=args.iterator_batch_size)
-    tokenizer = base_tokenizer.train_new_from_iterator(dataset_iterator, vocab_size=args.vocab_size)
+    dataset_iterator = get_dataset_iterator(
+        dataset["train"], batch_size=args.iterator_batch_size
+    )
+    tokenizer = base_tokenizer.train_new_from_iterator(
+        dataset_iterator, vocab_size=args.vocab_size, length=len(dataset)
+    )
     print(f"New tokenizer\n{tokenizer}")
+    args.target_model_path.parent.mkdir(parents=True, exist_ok=True)
     tokenizer.save_pretrained(args.target_model_path)
     print(f"Saved tokenizer to path: {args.target_model_path}")
+
 
 if __name__ == "__main__":
     args = get_args()
