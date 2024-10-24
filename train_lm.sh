fe66a403e6c6815d649179e7b426c5c3832c9a5f
e-hossam96 <ehh.hossam@gmail.com>
2024-10-24 08:50:28 +0000

    added train config for entire data

--- train_lm.sh
+++ train_lm.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-python src/preprocess_data.py
-python src/build_tokenizer.py
+# python src/preprocess_data.py
+# python src/build_tokenizer.py
 python src/train_causal_lm.py \
-    --split_size=200000 \
-    --num_epochs=2 \
+    --num_epochs=24 \
     --lr=1e-3 \
-    --run_name="Arabic-NanoGPT-LM-on-200K-Docs" \
+    --batch_size=64 \
+    --accum_steps=4 \
+    --eval_steps=1000 \
+    --log_steps=200 \
+    --run_name="Arabic-NanoGPT-LM-on-Wikipedia-Docs-23" \
     --notes="LM Training on Arabic Data using Nano GPT2 Model Architecture" \
-    --tags="Modeling,Transformers,GPT2,Language-Modeling,Arabic" \
+    --tags="Modeling,Transformers,GPT2,Language-Modeling,Arabic-Wikipedia" \
     --torch_compile
