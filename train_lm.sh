da56a98d2215a176047ab36a5ddfcccdeb425160
e-hossam96 <ehh.hossam@gmail.com>
2024-10-24 05:44:32 +0300

    changed default lr and notes in command

--- train_lm.sh
+++ train_lm.sh
@@ -4,7 +4,7 @@ python src/build_tokenizer.py
 python src/train_causal_lm.py \
     --split_size=200000 \
     --num_epochs=2 \
-    --lr=6e-4 \
+    --lr=1e-3 \
     --run_name="Arabic-NanoGPT-LM-on-200K-Docs" \
     --notes="LM Training on Arabic Data using Nano GPT2 Model Architecture" \
-    --tags=["MOdeling", "Transformers", "GPT2", "Language-Modeling", "Arabic"]
+    --tags="Modeling,Transformers,GPT2,Language-Modeling,Arabic"
