777bb5c1da258bb4aa3a5f4ed1847cab4c00c4e6
e-hossam96 <ehh.hossam@gmail.com>
2024-10-25 23:46:29 +0300

    added configs to fit small batch for v1 model

--- train_lm.sh
+++ train_lm.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
+
+model_name="arabic-nano-gpt-v1"
+
 # python src/preprocess_data.py
-# python src/build_tokenizer.py
+
+python src/build_tokenizer.py \
+    --model_name=$model_name
+
 python src/train_causal_lm.py \
-    --num_epochs=24 \
-    --lr=1e-3 \
-    --batch_size=64 \
-    --accum_steps=4 \
-    --eval_steps=1000 \
-    --log_steps=200 \
-    --run_name="Arabic-NanoGPT-LM-on-Wikipedia-Docs-23" \
+    --split_size=500 \
+    --n_embd=384 \
+    --n_head=6 \
+    --num_epochs=100 \
+    --lr=6e-4 \
+    --wd=0.0 \
+    --warmup=0.0 \
+    --batch_size=32 \
+    --accum_steps=1 \
+    --eval_steps=100 \
+    --log_steps=50 \
+    --model_name=$model_name \
+    --run_name="Overfitting-Small-Batch-v1" \
     --notes="LM Training on Arabic Data using Nano GPT2 Model Architecture" \
     --tags="Modeling,Transformers,GPT2,Language-Modeling,Arabic-Wikipedia" \
-    --torch_compile
