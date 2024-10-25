711e154ead942dbc92b99e1608807e91476c0f44
e-hossam96 <ehh.hossam@gmail.com>
2024-10-25 23:55:34 +0300

    changed configs for full data training

--- train_lm.sh
+++ train_lm.sh
@@ -2,24 +2,23 @@
 
 model_name="arabic-nano-gpt-v1"
 
-# python src/preprocess_data.py
+python src/preprocess_data.py
 
 python src/build_tokenizer.py \
     --model_name=$model_name
 
 python src/train_causal_lm.py \
-    --split_size=500 \
     --n_embd=384 \
     --n_head=6 \
-    --num_epochs=100 \
-    --lr=6e-4 \
-    --wd=0.0 \
-    --warmup=0.0 \
-    --batch_size=32 \
-    --accum_steps=1 \
-    --eval_steps=100 \
-    --log_steps=50 \
+    --num_epochs=24 \
+    --lr=2e-4 \
+    --wd=1e-5 \
+    --warmup=0.01 \
+    --batch_size=64 \
+    --accum_steps=8 \
+    --eval_steps=5000 \
+    --log_steps=2000 \
     --model_name=$model_name \
-    --run_name="Overfitting-Small-Batch-v1" \
+    --run_name="Arabic-NanoGPT-LM-on-Wikipedia-Docs-23-V1" \
     --notes="LM Training on Arabic Data using Nano GPT2 Model Architecture" \
-    --tags="Modeling,Transformers,GPT2,Language-Modeling,Arabic-Wikipedia" \
+    --tags="Modeling,Transformers,GPT2,Language-Modeling,Arabic-Wikipedia"
